df3b8068b87d8e233ac87dc239747763e7f67145
amine <aminejaji@gmail.com>
2019-03-24 20:21:09 +0100

    install body-parser

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ let exs = require('express')
 
 let app = exs()
 
-app.use(exs)
+app.use('/assets', exs.static('public'))
 
 app.set('view engine', 'ejs')
 
-app.get('/',(request,response) =>{
+app.get('/', (request, response) =>{
     //response.send('Hi Everyone')
     response.render('pages/index',{msg: 'Hi Everyone'})
+})
+
+app.post('/', (request, response) =>{
+    console.log(request.body)
 })
 
 app.listen(8080)
