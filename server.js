621aada35632f5aeb9a3a55dfafc1a6702f741bc
amine <aminejaji@gmail.com>
2019-03-24 15:50:32 +0100

    ejs values

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 let app = require('express')()
 
+app.set('view engine', 'ejs')
+
 app.get('/',(request,response) =>{
-    response.send('Hi Everyone')
+    //response.send('Hi Everyone')
+    response.render('pages/index',{msg: 'Hi Everyone'})
 })
 
 app.listen(8080)
